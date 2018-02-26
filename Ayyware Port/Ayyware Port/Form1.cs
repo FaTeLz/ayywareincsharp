af45ebc72943bf689409db5973da51c3d60c1855
FaTeLz <34930732+FaTeLz@users.noreply.github.com>
2018-02-25 21:32:00 -0500

    First release of the project
    Pushed first commit.

---
+++ Ayyware Port/Ayyware Port/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+namespace Ayyware_Port
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show(string.Format("Created By {0}", "FaTeLz"), "Ayyware");
+            this.TopMost = true;
+        }
+        #region movement
+        private Point mouseLocation;
+        private bool menumoving;
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics p;
+            p = e.Graphics;
+            p.DrawRectangle(new Pen(new LinearGradientBrush(new Point(3, 4), new Point(Width, 4), Color.Blue, Color.Fuchsia), 2), new Rectangle(3, 4, Width - 6, 1));
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && new Rectangle(0, 0, Width, 10).Contains(e.Location))
+            {
+                menumoving = true;
+                mouseLocation = e.Location;
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (menumoving)
+                Location = new Point(MousePosition.X - mouseLocation.X, MousePosition.Y - mouseLocation.Y);
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) menumoving = false;
+        }
+        #endregion
+    }
+}
---
+++ Ayyware Port/Ayyware Port/ayyware.cs
@@ -0,0 +1,382 @@
+﻿#region Imports
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.ComponentModel;
+
+#endregion
+
+
+namespace ayyware
+{
+    #region  CheckBox
+    [DefaultEvent("CheckedChanged")]
+    public class CCheckBox : Control
+    {
+
+        #region  Variables
+
+        private int X;
+        private bool _Checked = false;
+        private GraphicsPath Shape;
+
+        #endregion
+        #region  Properties
+
+        public bool Checked
+        {
+            get
+            {
+                return _Checked;
+            }
+            set
+            {
+                _Checked = value;
+                Invalidate();
+            }
+        }
+
+        #endregion
+        #region  EventArgs
+
+        public delegate void CheckedChangedEventHandler(object sender);
+        private CheckedChangedEventHandler CheckedChangedEvent;
+
+        public event CheckedChangedEventHandler CheckedChanged
+        {
+            add
+            {
+                CheckedChangedEvent = (CheckedChangedEventHandler)System.Delegate.Combine(CheckedChangedEvent, value);
+            }
+            remove
+            {
+                CheckedChangedEvent = (CheckedChangedEventHandler)System.Delegate.Remove(CheckedChangedEvent, value);
+            }
+        }
+
+
+        protected override void OnMouseMove(System.Windows.Forms.MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            X = e.Location.X;
+            Invalidate();
+        }
+        protected override void OnMouseDown(System.Windows.Forms.MouseEventArgs e)
+        {
+            _Checked = !_Checked;
+            Focus();
+            if (CheckedChangedEvent != null)
+                CheckedChangedEvent(this);
+            base.OnMouseDown(e);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            this.Height = 16;
+
+            Shape = new GraphicsPath();
+            Shape.AddArc(0, 0, 10, 10, 180, 90);
+            Shape.AddArc(Width - 11, 0, 10, 10, -90, 90);
+            Shape.AddArc(Width - 11, Height - 11, 10, 10, 0, 90);
+            Shape.AddArc(0, Height - 11, 10, 10, 90, 90);
+            Shape.CloseAllFigures();
+            Invalidate();
+        }
+
+        #endregion
+
+        public CCheckBox()
+        {
+            Width = 148;
+            Height = 16;
+            Font = new Font("Microsoft Sans Serif", 9);
+            DoubleBuffered = true;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics G = e.Graphics;
+            G.Clear(Parent.BackColor);
+
+            if (_Checked)
+            {
+                Rectangle fillRect = new Rectangle(4, 4, 10, 10);
+                LinearGradientBrush fillBrush = new LinearGradientBrush(fillRect, Color.FromArgb(188, 244, 33), Color.FromArgb(188, 244, 33), 0.0f, false);
+                G.FillRectangle(fillBrush, fillRect);
+            }
+            else
+            {
+                G.FillRectangle(new SolidBrush(Color.FromArgb(50, 50, 50)), new Rectangle(4, 4, 10, 10));
+            }
+
+            G.DrawString(Text, Font, new SolidBrush(Color.FromArgb(255, 255, 255)), new Point(20, 0));
+        }
+    }
+    #endregion
+    #region  TrackBar
+
+    [DefaultEvent("ValueChanged")]
+    public class CTrackBar : Control
+    {
+
+        #region  Enums
+
+        public enum ValueDivisor
+        {
+            By1 = 1,
+            By10 = 10,
+            By100 = 100,
+            By1000 = 1000
+        }
+
+        #endregion
+        #region  Variables
+
+        private Rectangle FillValue;
+        private Rectangle PipeBorder;
+        private Rectangle TrackBarHandleRect;
+        private bool Cap;
+        private int ValueDrawer;
+
+        private Size ThumbSize = new Size(14, 14);
+        private Rectangle TrackThumb;
+
+        private int _Minimum = 0;
+        private int _Maximum = 10;
+        private int _Value = 0;
+
+        private bool _JumpToMouse = false;
+        private ValueDivisor DividedValue = ValueDivisor.By1;
+
+        #endregion
+        #region  Properties
+
+        public int Minimum
+        {
+            get
+            {
+                return _Minimum;
+            }
+            set
+            {
+
+                if (value >= _Maximum)
+                {
+                    value = _Maximum - 10;
+                }
+                if (_Value < value)
+                {
+                    _Value = value;
+                }
+
+                _Minimum = value;
+                Invalidate();
+            }
+        }
+
+        public int Maximum
+        {
+            get
+            {
+                return _Maximum;
+            }
+            set
+            {
+
+                if (value <= _Minimum)
+                {
+                    value = _Minimum + 10;
+                }
+                if (_Value > value)
+                {
+                    _Value = value;
+                }
+
+                _Maximum = value;
+                Invalidate();
+            }
+        }
+
+        public delegate void ValueChangedEventHandler();
+        private ValueChangedEventHandler ValueChangedEvent;
+
+        public event ValueChangedEventHandler ValueChanged
+        {
+            add
+            {
+                ValueChangedEvent = (ValueChangedEventHandler)System.Delegate.Combine(ValueChangedEvent, value);
+            }
+            remove
+            {
+                ValueChangedEvent = (ValueChangedEventHandler)System.Delegate.Remove(ValueChangedEvent, value);
+            }
+        }
+
+        public int Value
+        {
+            get
+            {
+                return _Value;
+            }
+            set
+            {
+                if (_Value != value)
+                {
+                    if (value < _Minimum)
+                    {
+                        _Value = _Minimum;
+                    }
+                    else
+                    {
+                        if (value > _Maximum)
+                        {
+                            _Value = _Maximum;
+                        }
+                        else
+                        {
+                            _Value = value;
+                        }
+                    }
+                    Invalidate();
+                    if (ValueChangedEvent != null)
+                        ValueChangedEvent();
+                }
+            }
+        }
+
+        public ValueDivisor ValueDivison
+        {
+            get
+            {
+                return DividedValue;
+            }
+            set
+            {
+                DividedValue = value;
+                Invalidate();
+            }
+        }
+
+        [Browsable(false)]
+        public float ValueToSet
+        {
+            get
+            {
+                return _Value / (int)DividedValue;
+            }
+            set
+            {
+                Value = (int)(value * (int)DividedValue);
+            }
+        }
+
+        public bool JumpToMouse
+        {
+            get
+            {
+                return _JumpToMouse;
+            }
+            set
+            {
+                _JumpToMouse = value;
+                Invalidate();
+            }
+        }
+
+        #endregion
+        #region  EventArgs
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            checked
+            {
+                bool flag = this.Cap && e.X > -1 && e.X < this.Width + 1;
+                if (flag)
+                {
+                    this.Value = this._Minimum + (int)System.Math.Round((double)(this._Maximum - this._Minimum) * ((double)e.X / (double)this.Width));
+                }
+            }
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            if (e.Button == MouseButtons.Left)
+            {
+
+                this.ValueDrawer = (int)System.Math.Round(((double)(this._Value - this._Minimum) / (double)(this._Maximum - this._Minimum)) * (double)(this.Width - 11));
+                TrackBarHandleRect = new Rectangle(ValueDrawer, 0, 25, 25);
+                Cap = TrackBarHandleRect.Contains(e.Location);
+                Focus();
+                if (_JumpToMouse)
+                {
+                    this.Value = this._Minimum + (int)System.Math.Round((double)(this._Maximum - this._Minimum) * ((double)e.X / (double)this.Width));
+                }
+            }
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+            Cap = false;
+        }
+
+        #endregion
+
+        public CTrackBar()
+        {
+            SetStyle((System.Windows.Forms.ControlStyles)(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint | ControlStyles.ResizeRedraw | ControlStyles.DoubleBuffer), true);
+
+            Size = new Size(80, 22);
+            MinimumSize = new Size(47, 22);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            Height = 22;
+        }
+
+        protected override void OnPaint(System.Windows.Forms.PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics G = e.Graphics;
+
+            G.Clear(Parent.BackColor);
+            G.SmoothingMode = SmoothingMode.AntiAlias;
+            TrackThumb = new Rectangle(7, 10, Width - 16, 2);
+            PipeBorder = new Rectangle(1, 10, Width - 3, 2);
+
+            try
+            {
+                this.ValueDrawer = (int)System.Math.Round(((double)(this._Value - this._Minimum) / (double)(this._Maximum - this._Minimum)) * (double)(this.Width));
+            }
+            catch (Exception)
+            {
+            }
+
+            TrackBarHandleRect = new Rectangle(ValueDrawer, 0, 3, 20);
+
+            G.FillRectangle(new SolidBrush(Color.FromArgb(124, 131, 137)), PipeBorder);
+            FillValue = new Rectangle(0, 10, TrackBarHandleRect.X + TrackBarHandleRect.Width - 4, 3);
+
+            G.ResetClip();
+
+            G.SmoothingMode = SmoothingMode.Default;
+            G.DrawRectangle(new Pen(Color.FromArgb(50, 50, 50)), PipeBorder); // Draw pipe border
+            G.FillRectangle(new SolidBrush(Color.FromArgb(188, 244, 33)), FillValue);
+
+            G.ResetClip();
+
+            G.SmoothingMode = SmoothingMode.HighQuality;
+            G.DrawString(this.Value.ToString(), this.Font, new SolidBrush(this.ForeColor), new Point(this.TrackThumb.X + (int)System.Math.Round(unchecked((double)this.TrackThumb.Width * ((double)this.Value / (double)this.Maximum))) - (int)System.Math.Round((double)this.ThumbSize.Width / 2.0) + 2, this.TrackThumb.Y + (int)System.Math.Round((double)this.TrackThumb.Height / 2.0) - (int)System.Math.Round((double)this.ThumbSize.Height / 2.0)));
+        }
+    }
+    #endregion
+}
